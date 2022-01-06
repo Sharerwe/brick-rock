909d1f008baed88dd18e023ec5ae54f55f3b8115
Sharerwe <96344951+Sharerwe@users.noreply.github.com>
2022-01-06 16:38:07 -0500

    Update sleep.py

--- modules/sleep.py
+++ modules/sleep.py
@@ -3,8 +3,8 @@ import time
 import random
 
 def run(**args):
-    upper = 15
-    lower = 10
+    upper = 4
+    lower = 8
     sleep_time = random.randint(lower, upper)
     print('[!] Sleep:', sleep_time)
     time.sleep(sleep_time)
