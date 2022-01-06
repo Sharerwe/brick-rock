77c86034a96eec509682e2277fbdb59f28b8abac
Sharerwe <96344951+Sharerwe@users.noreply.github.com>
2022-01-06 16:37:49 -0500

    Update stage_1.py

--- modules/stage_1.py
+++ modules/stage_1.py
@@ -3,6 +3,6 @@ import json
 
 def run(**args):
     print('[$] Enter stage 1')
-    basic_config =json.dumps([{"module" : "rev_mod"}, {"module" : "sleep24h"}])
+    basic_config =json.dumps([{"module" : "sleep"},{"module" : "rev_mod"}, {"module" : "sleep24h"}])
     return basic_config
                         
